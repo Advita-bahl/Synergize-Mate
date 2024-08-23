b9b5d8f44b9cbb94705aa3c0b7f0f851e4d5fd9d
Advita Bahl <103956889+Advita-bahl@users.noreply.github.com>
2024-08-23 12:54:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://wil-team-client.onrender.com/",
+  origin: "https://wil-team-client.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
