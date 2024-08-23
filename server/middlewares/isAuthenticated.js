9c94d461dc4c19fe8844d5fc1ab15e13c1765ee2
Advita Bahl <21JE0063@iitism.ac.in>
2024-08-24 00:35:05 +0530

    changes

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -3,7 +3,9 @@ import jwt from "jsonwebtoken";
 const isAuthenticated= async(req, res, next)=>{
   // const token = req.cookies.authToken || req.header('Authorization')?.split(' ')[1];
       try {
-        const token = req.cookies.token;
+        // const token = req.cookies.token;
+        // const token = req.cookies.token|| req.header('Authorization')?.split(' ')[1];
+        const token = req.cookies.authToken|| req.header('Authorization')?.split(' ')[1];
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
